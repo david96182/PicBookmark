8a4dbc4bb039727628ec9ef44551e6be34e6418a
David Puerta Martín <puertadavid96182@gmail.com>
2023-08-30 15:54:05 -0400

    first commit

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
