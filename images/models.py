48f0b563014d38c8e95f9ecaec31fde1c0495611
David Puerta Martín <puertadavid96182@gmail.com>
2023-08-30 16:21:14 -0400

    create image model

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -12,6 +13,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
     
     class Meta:
         indexes = [
@@ -21,3 +25,8 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
