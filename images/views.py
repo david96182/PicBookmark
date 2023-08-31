c0b8b66f827ba3a0b3e7f030412051601031d233
David Puerta Martín <puertadavid96182@gmail.com>
2023-08-31 01:08:33 -0400

    add bookmarklet

--- images/views.py
+++ images/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -25,3 +27,10 @@ def image_create(request):
                      'images/image/create.html',
                      {'section': 'images',
                      'form': form})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                'images/image/detail.html',
+                {'section': 'images',
+                'image': image})
