e07b5ebfe572b38bdccf1aad9f645f6860618202
David Puerta Martín <puertadavid96182@gmail.com>
2023-09-05 09:56:06 -0400

    add activity stream for users

--- account/views.py
+++ account/views.py
@@ -7,6 +7,8 @@ from .models import Profile, Contact
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
+from actions.utils import create_action
+from actions.models import Action
 
 
 def user_login(request):
@@ -32,9 +34,19 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                        flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def register(request):
@@ -49,6 +61,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -112,6 +125,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(
                     user_from=request.user,
                     user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
--- images/views.py
+++ images/views.py
@@ -7,6 +7,7 @@ from .models import Image
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 @login_required
 def image_create(request):
@@ -20,6 +21,7 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, 'bookmarked image', new_image)
             messages.success(request, 'Image added successfully')
             # redirect to new created item detail view
             return redirect(new_image.get_absolute_url())
@@ -49,6 +51,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
