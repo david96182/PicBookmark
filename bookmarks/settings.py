0eefe4844a64d53df5be1ecd5682c35e799b82db
David Puerta Martín <puertadavid96182@gmail.com>
2023-09-05 22:29:58 -0400

    add debug-toolbar

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'images.apps.ImagesConfig',
     'easy_thumbnails',
     'actions.apps.ActionsConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -168,4 +170,8 @@ ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
 }
+
+INTERNAL_IPS = [
+'127.0.0.1',
+]
 
