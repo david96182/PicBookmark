6b51f1ac0a327b6c9c34fb6e139d6be220ded32a
David Puerta Martín <puertadavid96182@gmail.com>
2023-09-14 21:47:18 -0400

    update static settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -134,8 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
