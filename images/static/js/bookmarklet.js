e05939350ac9832751d766edac67a9ec76b533ce
David Puerta Martín <puertadavid96182@gmail.com>
2023-09-14 22:14:58 -0400

    update project url in js files

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,4 +1,4 @@
-const siteUrl = '//127.0.0.1:8000/';
+const siteUrl = '//picbookmark.onrender.com/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
